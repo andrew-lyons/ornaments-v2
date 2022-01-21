edb74a621ad1c17a2aa6e88635df1671b16a5c4d
Andrew Lyons <aslyons001@gmail.com>
2022-01-20 22:33:40 -0500

    starting mobile

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
   <main>
-    <h1>Welcome</h1>
-    <div class="red-orb" />
-    <div class="teal-orb" />
-    <div class="gold-orb" />
-    <div class="green-orb" />
-    <div class="purple-orb" />
+    <div class="bodytext">
+      <h1 class="text title">Welcome</h1>
+      <h2 class="text subtitle">This year, we honored 150 babies and donated $2,500 to Pregnancy + Infant Loss organization Sufficient Grace Ministries.</h2>
+      <h2 class="text">To combat P+I Loss stigma, and to help provide for those who need it, we are working on:</h2>
+      <ul>
+        <li>An online store for P+I Loss memorials and gifts</li>
+        <li>A community hub for support groups, and more</li>
+        <li>A resource guide for podcasts, books, and more</li>
+        <li>An organization directory for businesses and non-profits that can help or provide support</li>
+        <li>Faith-based options for all the above</li>
+        <li>And much more!</li>
+      </ul>
+
+      <div class="inputs">
+        <h2 class="text" style="margin: 0;">If you want to keep in touch...</h2>
+        <h4 class="italic" style="margin: 0;">(We don't send spam emails, promise!)</h4>
+        <input class="input-1" type="text" placeholder="Drop your email for updates!">
+        <input type="text" placeholder="Send us a message. (Optional)">
+      </div>
+      
+      <div class="button">
+        <a href="" class="submitEmail">Submit</a>
+      </div>
+    </div>
+      <lottie class="lottie-main-al" :options="defaultOptions" v-on:animCreated="handleAnimation"/>
   </main>
 </template>
 
@@ -15,24 +34,39 @@ if (!("scrollBehavior" in document.documentElement.style)) {
   import("scroll-behavior-polyfill");
 }
 
+import Lottie from 'vue-lottie';
+import animationData from './assets/json/peace.json';
+
 export default {
   name: 'App',
+  components: {
+    'lottie': Lottie
+  },
   data() {
     return {
-      
+      redSize: 24.3,
+      tealSize: 17.7,
+      goldSize: 16.8,
+      greenSize: 16.8,
+      purpleSize: 23.4,
+      incr: 1,
+      defaultOptions: {animationData: animationData},
+      animationSpeed: 1
     }
   },
-  methods: {},
+  methods: {
+      handleAnimation(anim) {
+        this.anim = anim;
+      }
+  },
   computed: {},
-  mounted() {
-
-  }
+  mounted() {}
 }
 </script>
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap');  
-
+  
   html, body {
     margin: 0;
     font-family: 'Quicksand', sans-serif;
@@ -40,112 +74,136 @@ export default {
   }
 
   html, body, main {
-    background: #131126;
+    background: rgb(255, 246, 217);
   }
-
 </style>
 
 <style scoped lang="scss">
   main {
     position: relative;
-    height: 100vh;
     width: 100vw;
+    height: 100vh;
     overflow: hidden;
+    display: flex;
 
-    --red-top: 15vh;
-    --red-left: 39vw;
-    --red-size: 24.3vw;
-    --red-filter: blur(75px);
-    --red-transform: rotate(95deg);
-
-    --teal-top: 46vh;
-    --teal-left: 36vw;
-    --teal-size: 17.7vw;
-    --teal-filter: blur(60px);
-    --teal-transform: rotate(91deg);
-
-    --gold-top: 32vh;
-    --gold-left: 50vw;
-    --gold-size: 16.8vw;
-    --gold-filter: blur(60px);
-    --gold-transform: rotate(40deg);
-
-    --green-top: 41vh;
-    --green-left: 50vw;
-    --green-size: 16.8vw;
-    --green-filter: blur(37px);
-    --green-transform: rotate(98deg);
-
-    --purple-top: 42vh;
-    --purple-left: 52vw;
-    --purple-size: 23.4vw;
-    --purple-filter: blur(40px);
-    --purple-transform: rotate(110deg);
+    @media (max-width: 1024px) {
+      flex-wrap: wrap;
+    }
   }
 
-  .red-orb {
-    position: absolute;
-    top: var(--red-top);
-    left: var(--red-left);
-    width: var(--red-size);
-    height: var(--red-size);
-    filter: var(--red-filter);
-    transform: var(--red-transform);
-    background: #FF4400;
-    mix-blend-mode: screen;
-    border-radius: 42%;
+  .bodytext {
+    display: flex;
+    flex-wrap: wrap;
+    align-content: flex-start;
+    position: relative;
+    z-index: 10;
+    width: 40%;
+    padding: 50px;
+    color: rgb(42, 59, 80);
+
+    @media (max-width: 1024px) {
+      width: 100%;
+      padding: 25px;
+    }
   }
 
-  .teal-orb {
-    position: absolute;
-    top: var(--teal-top);
-    left: var(--teal-left);
-    width: var(--teal-size);
-    height: var(--teal-size);
-    filter: var(--teal-filter);
-    transform: var(--teal-transform);
-    background: radial-gradient(217.68% 217.68% at 27.97% 49.05%, #5AE9FD 0%, rgba(90, 233, 253, 0) 100%);
-    mix-blend-mode: screen;
-    border: 20.8557px solid #00E29A;
-    border-radius: 177.496px;
+  .text {
+    margin: 0 0 12px 0;
   }
 
-  .gold-orb {
-    position: absolute;
-    top: var(--gold-top);
-    left: var(--gold-left);
-    width: var(--gold-size);
-    height: var(--gold-size);
-    filter: var(--gold-filter);
-    transform: var(--gold-transform);
-    background: conic-gradient(from 180deg at -3.84% 155.69%, #FFE072 0deg, rgba(255, 216, 75, 0) 360deg);
-    mix-blend-mode: screen;
-    border-radius: 243.066px;
+  .title {
+    font-size: 36px;
+    line-height: 36px;
+
+    @media (max-width: 1024px) {
+      font-size: 30px;
+      line-height: 30px;
+    }
   }
 
-  .green-orb {
-    position: absolute;
-    top: var(--green-top);
-    left: var(--green-left);
-    width: var(--green-size);
-    height: var(--green-size);
-    filter: var(--green-filter);
-    transform: var(--green-transform);
-    background: radial-gradient(50% 50% at -50% -35.62%, #38FF70 0%, rgba(74, 224, 116, 0.65) 100%);
-    mix-blend-mode: screen;
-    border-radius: 197.628px;
+  h2 {
+    @media (max-width: 1024px) {
+      font-size: 18px;
+      line-height: 20px;
+    }
   }
 
-  .purple-orb {
+  .italic {
+    font-style: italic;
+  }
+
+  .inputs {
+    input {
+      width: calc(100% - 12px);
+      padding: 12px 0 12px 12px;
+      border: none;
+      border-radius: 6px;
+      box-shadow: 2px 3px 3px 1px #2a3b50;
+    }
+
+    .input-1 {
+      margin-top: 24px;
+      margin-bottom: 12px;
+
+      @media (max-width: 1024px) {
+        margin-top: 12px;
+      }
+    }
+  }
+
+  ul {
+    margin-top: 0;
+  }
+
+  li {
+    font-size: 16px;
+    line-height: 24px;
+  }
+
+  li::marker {
+    color: #2a3b50;
+  }
+
+  .lottie-main-al {
+    width: calc(100vh * 1.136) !important;
+    height: 100vh !important;
+    margin: 0px !important;
     position: absolute;
-    top: var(--purple-top);
-    left: var(--purple-left);
-    width: var(--purple-size);
-    height: var(--purple-size);
-    filter: var(--purple-filter);
-    transform: var(--purple-transform);
-    background: radial-gradient(158.99% 50% at 50% 132.15%, #8B78FF 0%, rgba(99, 73, 255, 0.37) 100%);
-    mix-blend-mode: screen;
-    border-radius: 218.305px;
+    left: 40%;
+    z-index: 9;
+
+    @media (max-width: 1024px) {
+      position: relative;
+      width: 100vw !important;
+    }
+  }
+
+  .button {
+    width: 100%;
+    margin-top: 32px;
+
+    @media (max-width: 1024px) {
+      margin-top: 24px;
+    }
+  }
+
+  .submitEmail {
+    font-size: 24px;
+    line-height: 24px;
+    text-decoration: none;
+    color: #2a3b50;
+    padding: 12px;
+    border-radius: 12px;
+    background-color: #FFFFFF;
+    transition: background-color 0.25s, color 0.25s;
+
+    @media (max-width: 1024px) {
+      padding: 6px 12px;
+    }
+
+    &:hover {
+      background-color: #2a3b50;
+      color: #FFFFFF;
+    }
   }
 </style>
